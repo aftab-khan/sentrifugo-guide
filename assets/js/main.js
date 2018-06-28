a45b31fbd9aee96b21af5e57441048194d934cf7
Aftab Khan <aftabkhan@sapplica.com>
2018-06-28 18:30:54 +0530

    HTML Updates
    Added modal for images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $('main').hide().fadeIn(500);
 
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
@@ -12,79 +13,6 @@ $(document).ready(function() {
         $("html, body").animate({
             scrollTop: 0
         }, 600);
-        return false;
-    });
-
-    $('a[href^="#"]').on('click', function(e) {
-        $('.main_menu div a').each(function() {
-            $(this).removeClass('select');
-        })
-        $(this).addClass('select');
-    });
-
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-    });
-
-    // Get current page URL
-    var url = window.location.href;
-
-    // remove # from URL
-    url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
-
-    // remove parameters from URL
-    url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
-
-    // select file name
-    url = url.substr(url.lastIndexOf("/") + 1);
-
-    // If file name not avilable
-    if (url == '') {
-        url = 'index.html';
-    }
-
-    // Loop all menu items
-    $('.main_menu li').each(function() {
-        // select href
-        var href = $(this).find('a').attr('href');
-        // Check filename
-        if (url === href) {
-            // Add active class
-            $(this).addClass('select');
-        }
-    });
-
-    $('a[href*=#]').bind('click', function(e) {
-        e.preventDefault(); // prevent hard jump, the default behavior
-
-        var target = $(this).attr("href"); // Set the target as variable
-
-        // perform animated scrolling by getting top-position of target-element and set it as scroll target
-        $('html, body').stop().animate({
-            scrollTop: $(target).offset().top
-        }, 600, function() {
-            location.hash = target; //attach the hash (#jumptarget) to the pageurl
-        });
-
         return false;
     });
 });
@@ -107,3 +35,14 @@ $(window).scroll(function() {
         }
     });
 }).scroll();
+
+$(function() {
+    $("#mainmenuLoad").load("layout/main__menu.html"); 
+    $("#imageModal").load("layout/image__modal.html");
+
+    $('img.img-fluid').on('click', function() {
+        $('.enlargeImageModalSource').attr('src', $(this).attr('src'));
+        $('#enlargeImageModal').modal('show');
+    });
+    
+});
